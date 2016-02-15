1b82534270dd0f4d202f8777d9a18a36674ca61f
Miclilly <michellelilly87@hotmail.com>
2016-02-15 17:25:30 -0600

    hamburgermenu

--- site.js
+++ site.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
   $('.hamburger').on('click', function(){
-    console.log("HERe");
-    // if menu is close, slide down
-    $('.menu').slideToggle();
-    //if menu is open, slide up
-  })
+    $('.menu').toggleClass('open');
+  });
 });
